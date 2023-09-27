489074e4f9057720087525b6eb316940042990e0
philtechphilips <69679124+philtechphilips@users.noreply.github.com>
2023-09-27 22:28:11 +0100

    Active navlink implemented

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -14,3 +14,15 @@ document.addEventListener('keyup', function() {
   sidebar.classList.remove('active');
   closeSidebar.classList.remove('active');
 });
+
+
+// Get sactive navlink
+
+let currentURL = window.location.href;
+        let navLinks = document.querySelectorAll('.nav-links li a');
+        for (let i = 0; i < navLinks.length; i++) {
+            let link = navLinks[i];
+            if (link.href === currentURL) {
+                link.classList.add('active');
+            }
+        }
