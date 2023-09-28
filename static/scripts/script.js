c8d6742c4b5fed6aa8e063c7e87377b1f8f92fa7
philtechphilips <69679124+philtechphilips@users.noreply.github.com>
2023-09-28 10:18:43 +0100

    privacy policy page done

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -2,27 +2,32 @@ const sidebar = document.querySelector('.mobile-navigation-bar-links');
 const closeSidebar = document.querySelector('.close-sidebar');
 const menuIcon = document.querySelector('.menu-icon');
 
-menuIcon.addEventListener('click', function() {
+menuIcon.addEventListener('click', function () {
   sidebar.classList.toggle('active');
   closeSidebar.classList.toggle('active');
 });
-closeSidebar.addEventListener('click', function() {
+closeSidebar.addEventListener('click', function () {
   sidebar.classList.remove('active');
   closeSidebar.classList.remove('active');
 });
-document.addEventListener('keyup', function() {
+document.addEventListener('keyup', function () {
   sidebar.classList.remove('active');
   closeSidebar.classList.remove('active');
 });
 
 
-// Get sactive navlink
+// Get active navlink
 
 let currentURL = window.location.href;
-        let navLinks = document.querySelectorAll('.nav-links li a');
-        for (let i = 0; i < navLinks.length; i++) {
-            let link = navLinks[i];
-            if (link.href === currentURL) {
-                link.classList.add('active');
-            }
-        }
+let navLinks = document.querySelectorAll('.nav-links li a');
+for (let i = 0; i < navLinks.length; i++) {
+  let link = navLinks[i];
+  if (link.href === currentURL) {
+    link.classList.add('active');
+  }
+}
+
+
+
+
+
